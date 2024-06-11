982cd138545e6d5f0d7d31d2ba69341158f97242
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-06-11 15:01:34 -0400

    add: Loguru Logging refactor: Typer Commands (Docs) for Rabbit

--- server.py
+++ server.py
@@ -26,10 +26,15 @@ from rich.console import Console
 from rich.table import Table
 from rich.progress import Progress
 
+# Logger lib
+from loguru import Logger
+
+
 console = Console()
 app = typer.Typer()
 
-## Constants
+
+# Constants
 BACKLOG = 50
 MAX_THREADS = 200
 MAX_CHUNK_SIZE = 16 * 1024
@@ -38,19 +43,22 @@ MAX_REQUEST_SIZE = 1024
 BLACKLISTED_SITES = []
 WHITELISTED_SITES = []
 
+
 class Error(Enum):
     STATUS_500 = "Internal Server Error."
     STATUS_503 = "Service Unavailable."
     STATUS_505 = "HTTP Version Not Supported."
     STATUS_404 = "Not Found."
     STATUS_400 = "Bad Request."
-    
+
+
 class PROTOCOL(Enum):
     HTTP1_0 = "HTTP/1.0"
     HTTP1_1 = "HTTP/1.1"
     HTTP2_0 = "HTTP/2.0"
-    
-class HTTP_METHODS(Enum):
+
+
+class HTTPMETHODS(Enum):
     GET = "GET"
     PUT = "PUT"
     POST = "POST"
@@ -58,8 +66,9 @@ class HTTP_METHODS(Enum):
     HEAD = "HEAD"
     OPTIONS = "OPTIONS"
     CONNECT = "CONNECT"
-    
-class HTTP_STATUS(Enum):
+
+
+class HTTPSTATUS(Enum):
     OK = 200
     CREATED = 201
     ACCEPTED = 202
@@ -98,118 +107,251 @@ class HTTP_STATUS(Enum):
     GATEWAY_TIMEOUT = 504
     HTTP_VERSION_NOT_SUPPORTED = 505
     NETWORK_AUTHENTICATION_REQUIRED = 511
-    
-'''
+
+
+"""
 Request Class
-'''
+"""
+
+
 class Request:
     def __init__(self, raw: bytes) -> None:
         pass
-    
-'''
+
+
+""""
 Response Class
-'''
+"""
+
+
 class Response:
-    def __init__(self, status: HTTP_STATUS, body: str) -> None:
-        pass
-    
-'''
+    def __init__(self, status: HTTPSTATUS, body: str) -> None:
+        super().__init__()
+
+
+"""
 ConnectionHandler Class
-'''
+"""
+
+
 class ConnectionHandler(threading.Thread):
     def __init__(self, conn: socket.socket, addr) -> None:
-        super.__init__()
+        super().__init__()
         self.__connection = conn
         self.__address = addr
-    
+
     @property
     def connection(self):
         return self.__connection
-    
+
     @connection.setter
     def connection(self, connection: socket.socket):
         self.__connection = connection
-        
+
     @property
     def address(self):
         return self.__address
-    
+
     @address.setter
     def address(self, address):
-        self.__address = address 
-        
+        self.__address = address
+
     def run(self) -> None:
         raw_request = self.connection.recv(MAX_CHUNK_SIZE)
-        
+
         if not raw_request:
             self.connection.close()
             return
-        
-        
+
         req = Request(raw=raw_request)
-        
+
+
 class Server:
-    '''
-    Server Class
-    
-    :param host: str
-    :param port: int
-    
-    '''
     def __init__(self, host: str, port: int) -> None:
         self.__host = host
         self.__port = port
-    
-    
+
     def __start_server(self) -> None:
         pass
-    
-    
+
+
 @app.command()
-def start_server(host: str, port: Annotated[Optional[int], typer.Argument()] = 8080) -> None:
-    '''Command: start_server
-    
+def rabbit_start_server(host: str, port: Annotated[Optional[int], typer.Argument()] = 8080) -> None:
+    """Command: rabbit_start_server
+
     Parameters:
-    host: str - Hostname or IP address.
-    port: int - Port number, defaults to 8080.
-    
+        host: str - Hostname or IP address.
+        port: int - Port number, defaults to 8080.
+
     Description:
-    This command will start the server dependent on the host and port 
-    provided through the command line arguments.
-    '''
+        This command will start the server dependent on the host and port
+        provided through the command line arguments.
+
+    Returns:
+        None
+    """
+    pass
+
+
+@app.command()
+def rabbit_stop_server() -> None:
+    """Command: rabbit_stop_server
+
+    Description:
+        This command will stop any and all servers that are currently running,
+        with their defined host and port numbers.
+
+    Returns:
+    None
+    """
+    pass
+
+
+@app.command()
+def rabbit_restart_server(host: str, port: Annotated[Optional[int], typer.Argument()] = 8080) -> None:
+    """Command: rabbit_restart_server
+
+    Parameters:
+        host: str - Hostname or IP address.
+        port: int - Port number, defaults to 8080.
+
+    Description:
+        This command will restart the server dependent on the host and port
+        provided through the command line arguments.
+
+    Returns:
+        None
+    """
+    pass
+
+
+@app.command()
+def rabbit_status() -> None:
+    """Command: rabbit_status
+
+    Description:
+        This command will display the status of the current server, including
+        the number of active connections, the host and port number.
+
+    Returns:
+        None
+    """
+    pass
+
+
+@app.command()
+def rabbit_view_logs() -> None:
+    """Command: rabbit_view_logs
+
+    Description:
+        This command will display all active log files that are currently being
+        written to and updated by the server. During a connection, or request,
+        the server will write to a log file, which can be viewed using this command.
+
+        Alternatively, the log files can also be viewed by heading over to the
+        `logs` directory in the root of the project.
+
+    Returns:
+        None
+    """
+    pass
+
+
+@app.command()
+def rabbit_show_blacklisted() -> list[str]:
+    """Command: rabbit_show_blacklisted
+
+    Description:
+        This command will display all the blacklisted sites that are currently
+        being blocked by the server. The server will block any incoming requests
+        from the blacklisted sites, and will return a 403 Forbidden status code,
+        given that the site is blacklisted and isn't manually whitelisted.
+
+    Returns:
+        list[str]
+    """
+    pass
+
+
+@app.command()
+def rabbit_show_whitelisted() -> list[str]:
+    """Command: rabbit_show_whitelist
+
+    Description:
+        This command will display all the whitelisted sites that are currently
+        allowed by the server instance. The server will allow any incoming requests
+        from whitelisted sites, and will return a 200 OK status code, given that
+        the site is whitelisted.
+
+    Returns:
+        list[str]
+    """
     pass
 
 @app.command()
-def stop_server() -> None:
+def rabbit_blacklist(site: str) -> HTTPSTATUS.FORBIDDEN:
+    """Command: rabbit_blacklist
+
+    Parameters:
+        site: str - The site that you want to blacklist.
+
+    Description:
+        This command will blacklist a site, and block any incoming requests from
+        the site. The server will return a 403 Forbidden status code, given that
+        the site is blacklisted and isn't manually whitelisted.
+
+    Returns:
+        HTTPSTATUS.FORBIDDEN
+    """
     pass
 
 @app.command()
-def restart_server() -> None:
+def rabbit_whitelist(site: str) -> HTTPSTATUS.OK:
+    """Command: rabbit_whitelist
+
+    Parameters:
+        site: str - The site that you want to whitelist.
+
+    Description:
+        This command will whitelist a site, and allow any incoming requests from
+        the site. The server will return a 200 OK status code, given that the site
+        is whitelisted.
+
+    Returns:
+        HTTPSTATUS.OK
+    """
     pass
 
-@app.command()
-def status() -> None:
-    pass
 
 @app.command()
-def logs() -> None:
+def rabbit_display_config() -> None:
+    """Command: rabbit_config
+
+    Description:
+        This command will display the current configuration of the server, including
+        but not limited to the host, port, blacklisted sites, whitelisted sites, and
+        the number of active connections.
+
+    Returns:
+        None
+    """
     pass
 
-@app.command()
-def blacklist() -> None:
-    pass
 
 @app.command()
-def whitelist() -> None:
+def rabbit_help() -> None:
+    """Command: rabbit_help
+
+    Description:
+        This command will display all the available commands and their respective
+        descriptions, including the parameters that they accept, this is useful for
+        understanding the functionality of each command and how to use them.
+
+    Returns:
+        None
+    """
     pass
 
-@app.command()
-def config() -> None:
-    pass
-
-@app.command()
-def help() -> None:
-    pass
 
 if __name__ == '__main__':
     app()
